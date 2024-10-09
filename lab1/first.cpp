d89cee4f7741a282d13c6642ec066a09e4a4b69d
Mykola Tatarenkov <tatarmy4474_aehit@students.vizja.pl>
2024-10-09 15:43:12 +0300

    lab01

---
+++ lab1/first.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void greeting();
+
+int main()
+{
+    int x;
+    float y;
+    char z;
+    bool t;
+
+    greeting();
+
+    cin >> x;
+    if (x <0)
+    {
+
+    } else
+    {
+
+    }
+    cout << x;
+
+}
+
+void greeting()
+{
+    cout << "Hello" << endl;
+}
